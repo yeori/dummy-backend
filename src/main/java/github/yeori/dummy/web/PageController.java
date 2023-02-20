a0ed9ca065c6ff932c3f68b008df30a46f6e0d71
yeori <yeori.seo@gmail.com>
2023-02-20 14:43:49 +0900

    page mapping

--- src/main/java/github/yeori/dummy/web/PageController.java
+++ src/main/java/github/yeori/dummy/web/PageController.java
@@ -1,9 +1,14 @@
 package github.yeori.dummy.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class PageController {
 
+	@GetMapping("/")
+	public String pageMain() {
+		return "index";
+	}
 	
 }
